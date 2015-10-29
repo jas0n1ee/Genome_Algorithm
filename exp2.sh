a893ad3f65172718cfc786a96a407f76d0bf1694
jas0n1ee <i@jas0n1ee.me>
2015-10-28 23:16:13 -0700

    update

--- exp2.sh
+++ exp2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 time ./compress_fasta_hil.py tair8.fasta tair9.fasta hilbert.csv
-./x265_mod --input output.yuv --input-res 4096x4096 --fps 2 -p medium --qp $1  -o genome.265 --no-wpp --no-deblock --no-sao --psy-rd 0 --tune psnr --psnr
+./x265_mod --input output.yuv --input-res 4096x4096 --fps 2 -p veryslow --qp $1  -o genome.265 --no-wpp --no-deblock --no-sao --psy-rd 0 --tune psnr --psnr
 ffmpeg -i genome.265 -y genome.yuv > /dev/null 2>&1
 time ./decompress_hil.py 4096 4096 genome.yuv hilbert.csv
 ./compare.py tair8.txt decomp_a.txt
---
+++ gen_hilbert.m
@@ -0,0 +1,2 @@
+h12 = hilbert(12);
+dlmwrite('hilbert.csv', h12(:), 'delimiter', ',', 'precision', 9); 
