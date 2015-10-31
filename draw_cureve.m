f22caeba308eaf80017b898e898b478b96cf8f9e
jas0n1ee <i@jas0n1ee.me>
2015-10-30 23:46:02 -0700

    update

--- draw_cureve.m
+++ draw_cureve.m
@@ -1,8 +1,9 @@
 function draw_cureve(m)
     w = size(m,1);
+    h = size(m,2);
     x=[];
     y=[];
-    for i = 1:w^2
+    for i = 1:w*h
         [r c] = find(m==i);
         x = [x r];
         y = [y c];
